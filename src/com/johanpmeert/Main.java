618f2fa1949828c22f6eb06b19d6b7618c0033c0
johanpmeert <johan_meert@skynet.be>
2022-02-21 10:52:19 +0100

    Integrated auto host ip address

--- src/com/johanpmeert/Main.java
+++ src/com/johanpmeert/Main.java
@@ -6,17 +6,19 @@ import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
-import static javax.xml.bind.DatatypeConverter.parseHexBinary;
-import static javax.xml.bind.DatatypeConverter.printHexBinary;
-
 public class Main {
 
     public static void main(String[] args) {
 
         final String sma_multicastIp = "239.12.255.254";
         final int sma_multicastPort = 9522;
-        final String myHostIpAddress = "192.168.2.147";  // change here to your PC ip address
 
+        String myHostIpAddress = null;
+        try {
+            myHostIpAddress = Inet4Address.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            System.out.println("Host ip address could not be found");
+        }
         System.out.println("SMA discovery and readout tool for SMA energy meter and SMA home manager 2");
         System.out.print("Opening multicast socket from " + myHostIpAddress + "... ");
 
@@ -28,8 +30,8 @@ public class Main {
             mcSocket.joinGroup(group, netIf);
             System.out.println("done");
 
-            byte[] txbuf = parseHexBinary("534d4100000402a0ffffffff0000002000000000");  // discovery string to be sent to network, all SMA devices will answer
-            System.out.println("Sending out SMA specific discovery code " + printHexBinary(txbuf) + " to multicast address " + sma_multicastIp + "/" + sma_multicastPort);
+            byte[] txbuf = hexStringToByteArray("534d4100000402a0ffffffff0000002000000000");  // discovery string to be sent to network, all SMA devices will answer
+            System.out.println("Sending out SMA specific discovery code " + byteArrayToHexString(txbuf) + " to multicast address " + sma_multicastIp + "/" + sma_multicastPort);
             DatagramPacket data = new DatagramPacket(txbuf, txbuf.length, mcastAddr, sma_multicastPort);
             mcSocket.send(data);
 
@@ -37,13 +39,10 @@ public class Main {
             data = new DatagramPacket(buffer, buffer.length);
             while (true) {
                 mcSocket.receive(data);
-                // String hexdata = printHexBinary(buffer);
-                // hexdata = hexdata.substring(0, data.getLength() * 2);
-                // System.out.println("Received from " + data.getAddress() + ": " + hexdata);
                 byte[] slice = Arrays.copyOfRange(buffer, 0, data.getLength());
                 smaResponseData smaR = parseSmaResponse(slice);
                 if (smaR != null) {
-                    System.out.print("Device n° " + smaR.serial + " reports: P = ");
+                    System.out.print("Device serial n° " + smaR.serial + " reports: P = ");
                     System.out.print(smaR.power3f + " W");
                     System.out.print(" (" + smaR.powerL1 + "," + smaR.powerL2 + "," + smaR.powerL3 + ")");
                     System.out.println();
@@ -62,8 +61,7 @@ public class Main {
         // For the measurement we search the hexData for a specific marker and then extract from 4 to 8 bytes further the data
         // These markers are stored in the enum internalData
 
-        String hexString = printHexBinary(hexData);
-        int location;
+        String hexString = byteArrayToHexString(hexData);
         if (hexString.length() < 1200) return null;
         else {
             smaResponseData smar = new smaResponseData();
@@ -80,41 +78,32 @@ public class Main {
             // to make searching the marker position easier to program, we convert the byte[] to a hexString and look for the markers in this String
             // the extraction is done from the byte[] with the search result from above (divided by 2)
             // for correctness, the result is stored in a BigDecimal
-            location = hexString.indexOf(internalData.power3fpos.code) / 2;
-            byte[] result = Arrays.copyOfRange(hexData, location + 4, location + 8);
-            int power3fp = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
-            location = hexString.indexOf(internalData.power3fneg.code) / 2;
-            int power3fn = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
+            int power3fp = getValueFromMarker(hexData, internalData.power3fpos);
+            int power3fn = getValueFromMarker(hexData, internalData.power3fneg);
             if (power3fp != 0) {
                 smar.power3f = BigDecimal.valueOf(power3fp).divide(BigDecimal.TEN);
             } else {
                 smar.power3f = BigDecimal.valueOf(-power3fn).divide(BigDecimal.TEN);
             }
 
-            location = hexString.indexOf(internalData.powerL1pos.code) / 2;
-            int powerL1p = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
-            location = hexString.indexOf(internalData.powerL1neg.code) / 2;
-            int powerL1n = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
+            int powerL1p = getValueFromMarker(hexData, internalData.powerL1pos);
+            int powerL1n = getValueFromMarker(hexData, internalData.powerL1neg);
             if (powerL1p != 0) {
                 smar.powerL1 = BigDecimal.valueOf(powerL1p).divide(BigDecimal.TEN);
             } else {
                 smar.powerL1 = BigDecimal.valueOf(-powerL1n).divide(BigDecimal.TEN);
             }
 
-            location = hexString.indexOf(internalData.powerL2pos.code) / 2;
-            int powerL2p = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
-            location = hexString.indexOf(internalData.powerL2neg.code) / 2;
-            int powerL2n = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
+            int powerL2p = getValueFromMarker(hexData, internalData.powerL2pos);
+            int powerL2n = getValueFromMarker(hexData, internalData.powerL2neg);
             if (powerL2p != 0) {
                 smar.powerL2 = BigDecimal.valueOf(powerL2p).divide(BigDecimal.TEN);
             } else {
                 smar.powerL2 = BigDecimal.valueOf(-powerL2n).divide(BigDecimal.TEN);
             }
 
-            location = hexString.indexOf(internalData.powerL3pos.code) / 2;
-            int powerL3p = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
-            location = hexString.indexOf(internalData.powerL3neg.code) / 2;
-            int powerL3n = ByteBuffer.wrap(Arrays.copyOfRange(hexData, location + 4, location + 8)).getInt();
+            int powerL3p = getValueFromMarker(hexData, internalData.powerL3pos);
+            int powerL3n = getValueFromMarker(hexData, internalData.powerL3neg);
             if (powerL3p != 0) {
                 smar.powerL3 = BigDecimal.valueOf(powerL3p).divide(BigDecimal.TEN);
             } else {
@@ -123,6 +112,13 @@ public class Main {
 
             return smar;
         }
+    }
+
+    public static int getValueFromMarker(byte[] hexData, internalData marker) {
+        String hexDataString = byteArrayToHexString(hexData);
+        int markerLocation = hexDataString.indexOf(marker.code) / 2;
+        if (markerLocation == -1) return 0;
+        return ByteBuffer.wrap(Arrays.copyOfRange(hexData, markerLocation + marker.offset, markerLocation + marker.offset + marker.length)).getInt();
     }
 
     public static class smaResponseData {
@@ -135,21 +131,48 @@ public class Main {
 
     enum internalData {
         // These are the 4 byte markers in hex
-        power3fpos("00010400"),
-        power3fneg("00020400"),
-        powerL1pos("00150400"),
-        powerL1neg("00160400"),
-        powerL2pos("00290400"),
-        powerL2neg("002A0400"),
-        powerL3pos("003D0400"),
-        powerL3neg("003E0400");
+        power3fpos("00010400", 4, 4),
+        power3fneg("00020400", 4, 4),
+        powerL1pos("00150400", 4, 4),
+        powerL1neg("00160400", 4, 4),
+        powerL2pos("00290400", 4, 4),
+        powerL2neg("002A0400", 4, 4),
+        powerL3pos("003D0400", 4, 4),
+        powerL3neg("003E0400", 4, 4);
 
         String code;
+        int offset;
+        int length;
 
-        internalData(String code) {
+
+        internalData(String code, int offset, int length) {
             this.code = code;
+            this.offset = offset;
+            this.length = length;
         }
 
+    }
+
+    public static byte[] hexStringToByteArray(String hex) {
+        hex = hex.length() % 2 != 0 ? "0" + hex : hex;
+        byte[] b = new byte[hex.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+            int index = i * 2;
+            int v = Integer.parseInt(hex.substring(index, index + 2), 16);
+            b[i] = (byte) v;
+        }
+        return b;
+    }
+
+    public static String byteArrayToHexString(byte[] bytes) {
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
 }
